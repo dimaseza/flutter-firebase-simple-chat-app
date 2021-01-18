b71e507db90999c82f7061175d35979383e2f3d9
dimaseza <dimasandre18@gmail.com>
2021-01-19 05:05:21 +0700

    add stream builder and check user is login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_crew_course/pages/sign_up_page.dart';
 import 'package:flutter_chat_crew_course/services/auth_services.dart';
+import 'pages/sign_in_page.dart';
 import 'package:get/get.dart';
-import 'package:provider/provider.dart';
+import 'pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,14 +14,19 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider.value(
-        value: AuthServices.firebaseUserStream,
+    return GetMaterialApp(
+      title: "Chat App",
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+        stream: AuthServices.firebaseUserStream,
         builder: (context, snapshot) {
-          return GetMaterialApp(
-            title: "Chat App",
-            debugShowCheckedModeBanner: false,
-            home: SignUpPage(),
-          );
-        });
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return SignInPage();
+          }
+        },
+      ),
+    );
   }
 }
